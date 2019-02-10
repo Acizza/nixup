efd676fcd8c4327f84a9c54d50a992466b7909b4
Acizza <jgit@tuta.io>
2019-02-10 15:42:33 -0800

    [main] Add colored output

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod store;
 use crate::error::Error;
 use crate::store::StorePath;
 use clap::clap_app;
+use colored::Colorize;
 use std::collections::HashMap;
 use std::fs::{self, File};
 use std::path::PathBuf;
@@ -62,14 +63,57 @@ fn save_system_packages() -> Result<(), Error> {
 fn detect_package_diff() -> Result<(), Error> {
     let old_stores = load_saved_system_stores()?;
     let current_stores = store::get_system_pkg_stores()?;
-
     let diffs = store::get_store_diffs(&current_stores, &old_stores);
 
+    println!(
+        "{} system package(s) upgraded\n",
+        diffs.len().to_string().blue()
+    );
+
     for diff in &diffs {
-        println!("{}: {} -> {}", diff.name, diff.ver_from, diff.ver_to);
+        let ver_to_str = if cfg!(not(no_colors)) {
+            bolden_str_diff(&diff.ver_from, &diff.ver_to)
+        } else {
+            diff.ver_to.green().to_string()
+        };
+
+        println!(
+            "{}: {} -> {}",
+            diff.name.blue(),
+            diff.ver_from.red(),
+            ver_to_str
+        );
     }
 
     Ok(())
+}
+
+fn bolden_str_diff<S>(from: S, to: S) -> String
+where
+    S: AsRef<str>,
+{
+    let from = from.as_ref();
+    let to = to.as_ref();
+
+    let mut result = Vec::with_capacity(to.len());
+    let mut from_chars = from.chars();
+
+    for to_ch in to.chars() {
+        let from_ch = from_chars.next();
+        let to_str = to_ch.to_string().green();
+
+        if let Some(from_ch) = from_ch {
+            if from_ch == to_ch {
+                result.push(to_str.to_string());
+                continue;
+            }
+        }
+
+        let to_str = to_str.bright_green().underline().to_string();
+        result.push(to_str);
+    }
+
+    result.join("")
 }
 
 fn load_saved_system_stores() -> Result<HashMap<String, StorePath>, Error> {
